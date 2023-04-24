9ad21bb693150cac41e5d9f3da906b010d279b60
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-25 01:59:50 +0200

    fix: rating mapper

--- src/main/java/com/andreidodu/mapper/RatingMapper.java
+++ src/main/java/com/andreidodu/mapper/RatingMapper.java
@@ -2,6 +2,7 @@ package com.andreidodu.mapper;
 
 import com.andreidodu.dto.RatingDTO;
 import com.andreidodu.model.Rating;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,15 @@ public class RatingMapper extends ModelMapperCommon<Rating, RatingDTO> {
 
     public RatingMapper() {
         super(Rating.class, RatingDTO.class);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        super.getModelMapper().typeMap(Rating.class, RatingDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getUserTarget().getId(), RatingDTO::setUserTargetId);
+            mapper.map(src -> src.getUserVoter().getId(), RatingDTO::setUserVoterId);
+            mapper.map(src -> src.getJobInstance().getId(), RatingDTO::setJobInstanceId);
+        });
     }
 
 }
