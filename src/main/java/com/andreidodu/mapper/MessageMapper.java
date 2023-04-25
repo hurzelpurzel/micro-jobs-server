aacedede8b620590d2fa7ce9ae9b19bbb3326e60
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-25 02:41:16 +0200

    fix: message mapper

--- src/main/java/com/andreidodu/mapper/MessageMapper.java
+++ src/main/java/com/andreidodu/mapper/MessageMapper.java
@@ -2,6 +2,7 @@ package com.andreidodu.mapper;
 
 import com.andreidodu.dto.MessageDTO;
 import com.andreidodu.model.Message;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,4 +12,11 @@ public class MessageMapper extends ModelMapperCommon<Message, MessageDTO> {
         super(Message.class, MessageDTO.class);
     }
 
+    @PostConstruct
+    public void postConstruct() {
+        super.getModelMapper().typeMap(Message.class, MessageDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getUserFrom().getId(), MessageDTO::setUserFromId);
+            mapper.map(src -> src.getUserTo().getId(), MessageDTO::setUserToId);
+        });
+    }
 }
