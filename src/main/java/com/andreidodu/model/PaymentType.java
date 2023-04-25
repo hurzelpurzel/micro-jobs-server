e8aeaa9cb1e466bbd118af6bb073a2d4259f0846
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-25 03:37:54 +0200

    feature: add column payment type

--- src/main/java/com/andreidodu/model/PaymentType.java
+++ src/main/java/com/andreidodu/model/PaymentType.java
@@ -10,11 +10,10 @@ public class PaymentType extends ModelCommon {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
     @Column(name = "paypal_email", nullable = false, unique = true)
     private String paypalEmail;
-
-
+    @Column(name = "type", nullable = false)
+    private Integer type;
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
