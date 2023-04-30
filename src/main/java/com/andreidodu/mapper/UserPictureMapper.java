3e6003436c41fa19bee4aa124948e2613c5ee677
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-30 17:39:42 +0200

    refactor

--- src/main/java/com/andreidodu/mapper/UserPictureMapper.java
+++ src/main/java/com/andreidodu/mapper/UserPictureMapper.java
@@ -13,17 +13,4 @@ public class UserPictureMapper extends ModelMapperCommon<UserPicture, UserPictur
         super(UserPicture.class, UserPictureDTO.class);
     }
 
-    @PostConstruct
-    private void postConstruct() {
-
-//        getModelMapper().typeMap(UserPictureDTO.class, UserPicture.class).addMappings(mapper -> {
-//            mapper.using(ConverterCommon.STRING_TO_BYTES).<Byte[]>map(UserPictureDTO::getPicture, UserPicture::setPicture);
-//        });
-//
-//        getModelMapper().typeMap(UserPicture.class, UserPictureDTO.class).addMappings(mapper -> {
-//            mapper.using(ConverterCommon.BYTES_TO_STRING).<String>map(UserPicture::getPicture, UserPictureDTO::setPicture);
-//            mapper.map(src -> src.getUser().getId(), UserPictureDTO::setUserId);
-//        });
-    }
-
 }
